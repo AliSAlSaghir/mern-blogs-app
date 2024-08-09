549bd9898d447f255424168192fe271a73ed6721
Ali AlSaghir <alisalsaghir@gmail.com>
2024-08-09 12:28:49 +0300

    add error-handling middleware and error handlers

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,25 +1,17 @@
 import User from "../models/userModel.js";
 import bcryptjs from "bcryptjs";
+import errorHandler from "../utils/error.js";
+import catchAsync from "../utils/catchAsync.js";
 
-export const signup = async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-    if (
-      !username ||
-      !email ||
-      !password ||
-      username === "" ||
-      email === "" ||
-      password === ""
-    )
-      return res.status(400).json({ message: " All fields are required!" });
-
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({ username, email, hashedPassword });
-
-    await newUser.save();
-    res.json({ message: "Successfully Signed in" });
-  } catch (err) {
-    res.status(400).json(err.message);
+export const signup = catchAsync(async (req, res, next) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    return next(errorHandler(400, "All fields are required"));
   }
-};
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+
+  await newUser.save();
+  res.json({ message: "Successfully Signed in" });
+});
--- backend/index.js
+++ backend/index.js
@@ -16,3 +16,13 @@ app.use("/api/v1/auth", authRoutes);
 
 const port = process.env.PORT || 5001;
 app.listen(port, (req, res) => console.log("Server running on port 5001"));
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
