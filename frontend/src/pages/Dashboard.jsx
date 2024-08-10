71e4c811d12f0d67b57cdb76061e9c66d83393ff
Ali AlSaghir <alisalsaghir@gmail.com>
2024-08-10 12:03:01 +0300

    complete dashbord sidebar

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    tabFromUrl && setTab(tabFromUrl);
+  }, [location.search]);
+  return (
+    <div className="flex flex-col min-h-screen md:flex-row">
+      <div className="md:w-56">
+        <DashSidebar />
+      </div>
+      {tab === "profile" && <DashProfile />}
+    </div>
+  );
 };
 
 export default Dashboard;
